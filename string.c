9abe27c70a67196666fd489f7ebb50dabad09ad4
anushasingh0703 <122735664+anushasingh0703@users.noreply.github.com>
2023-03-26 12:31:28 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	char name[]="ANUSHA";
+	printf("%s",name);
+	return 0;
+}
